fea1cdc8100fd3368b9c3a76b95cb6536775c5fd
basim jawad <basemjm998@gmail.com>
2024-04-24 16:00:38 +0300

    add FCM

--- socket/socket.js
+++ socket/socket.js
@@ -1,6 +1,36 @@
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
+const admin = require("firebase-admin");
+const serverAccount = require("C:\\Users\\ALBATOOL\\Downloads\\chatapp\\chat-app-socketio-71eba-firebase-adminsdk-lwlg4-c656e8b6c1.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serverAccount),
+});
+
+const userToken =
+  "foIh1cqaQhmI9wE8-eg4sc:APA91bE7X7EsOx6D_GiDmYM0_HtHkeEcIcjmGQ-A5c7vBzjBms3eyFnAa3O6ApvVZue6X7Rk0jN4KVTLuRKyFhlnfNtMMbCqmkdVw94c5KBWFvtKrVwfSINnnqnSmKi_y2oyxbUdWc45";
+
+// Function to send notification to a specific user
+async function sendTestNotification(title, body) {
+  const payload = {
+    notification: {
+      title: title,
+      body: body,
+    },
+    token: userToken, // Use the user's token here
+  };
+
+  admin
+    .messaging()
+    .send(payload)
+    .then((response) => {
+      console.log("Successfully sent message:", response);
+    })
+    .catch((error) => {
+      console.log("Error sending message:", error);
+    });
+}
 
 const app = express();
 
@@ -23,6 +53,7 @@ io.on("connection", (socket) => {
   console.log("user login");
   socket.on("message", (message) => {
     console.log(message);
+    sendTestNotification("new Chat", message.text);
     io.emit("message", {
       message: message.text,
       senderId: message.senderId,
